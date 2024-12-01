5c7311fffd801876631bedc02b7b3bb6ba093948
Jaeyoung <jyko1101@gmail.com>
2024-12-01 13:41:19 +0900

    add: scene 1.Lobby

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -13,26 +14,48 @@ public class NetworkManager : MonoBehaviour
 {
     const string baseUrl = "http://localhost:3019/api/";
 
+    public void PingTest()
+    {
+        StartCoroutine(PingTestCoroutine());
+    }
+
+    private IEnumerator PingTestCoroutine()
+    {
+        // url: 기본 포트 + ping
+        StringBuilder urlBuilder = new StringBuilder(baseUrl).Append("ping");
+        string url = urlBuilder.ToString();
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        yield return www.SendWebRequest();
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Ping failed: " + www.error);
+        }
+        else
+        {
+            Debug.Log("Ping successful: " + www.downloadHandler.text);
+        }
+    }
 
     public void PostRequest(string endPoint, string json, System.Action<string> callback)
     {
-        Debug.Log("start PostRequestCoroutine()");
+        // POST 메서드 처리
         StartCoroutine(PostRequestCoroutine(endPoint, json, callback));
     }
 
     private IEnumerator PostRequestCoroutine(string endPoint, string json, System.Action<string> callback)
     {
+        // url: 기본 포트 + 앤드포인트로 지정
         StringBuilder urlBuilder = new StringBuilder(baseUrl).Append(endPoint);
         string url = urlBuilder.ToString();
-
+        // serialize 
         UnityWebRequest www = new UnityWebRequest(url, "POST");
         byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(json);
         www.uploadHandler = new UploadHandlerRaw(bodyRaw);
         www.downloadHandler = new DownloadHandlerBuffer();
         www.SetRequestHeader("Content-Type", "application/json");
-
+        // 서버에 req 전송, res 대기
         yield return www.SendWebRequest();
-
+        // res 처리 로직
         if (www.result != UnityWebRequest.Result.Success)
         {
             Debug.LogError("Error: " + www.error);
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # FutsalOnline-UnityClient
- 풋살온라인 프로젝트 클라이언트단 w. Unity Engine
+
+<br>
+
+풋살온라인 프로젝트 클라이언트단 w. Unity Engine
+
+<br>
+
+#### FutsalOnline 서버와 연결되는 테스트 프로토타입용 유니티 클라이언트
+
+<br>
+
+## 사용 개발환경
+- editor version: Unity 2022.3.46f1
+
+<br>
+
+> client for server api test
+
+따라서, 유니티 클라이언트 안에서의 좋은 UX 등은 고려하지 않고, API가 정상적으로 동작하는지만 확실하게 체크하기 위함.
+
