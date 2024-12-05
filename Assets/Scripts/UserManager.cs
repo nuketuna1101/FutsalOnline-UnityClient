979fa1eb88a33e35020258ccdd9423e025896849
Jaeyoung <jyko1101@gmail.com>
2024-12-06 01:09:28 +0900

    feat: userManager

--- Assets/Scripts/UserManager.cs
+++ Assets/Scripts/UserManager.cs
@@ -6,79 +6,42 @@ using TMPro;
 using UnityEngine.Networking;
 using System.Net;
 /// <summary>
-/// UserManager : 유저 회원 가입, 로그인 로직 처리
+/// UserManager : 유저 상태, 토큰 관리
 /// </summary>
 
 public class UserManager : MonoBehaviour
 {
-    public TMP_InputField usernameInput;
-    public TMP_InputField nicknameInput;
-    public TMP_InputField passwordInput;
-    public TMP_InputField confirmPasswordInput;
-    public Button signUpButton;
-    public NetworkManager networkManager;
-    private bool isServerRunning = false;
+    public static UserManager Instance { get; private set; }
 
-    void Start()
-    {
-        signUpButton.onClick.AddListener(() => StartCoroutine(SignUpCoroutine()));
-        PingTest();
-    }
+    public string UserId { get; private set; }
+    public string AuthToken { get; private set; }
+    public bool IsLoggedIn { get; private set; }
 
-    private IEnumerator SignUpCoroutine()
+    private void Awake()
     {
-        // inputfield 값 가져오기
-        string username = usernameInput.text;
-        string nickname = nicknameInput.text;
-        string password = passwordInput.text;
-        string confirmPassword = confirmPasswordInput.text;
-        // DTO 따라 JSON
-        SignUpDTO signUpData = new SignUpDTO
+        if (Instance == null)
         {
-            userName = usernameInput.text,
-            nickname = nicknameInput.text,
-            password = passwordInput.text,
-            confirmPassword = confirmPasswordInput.text
-        };
-        string json = JsonUtility.ToJson(signUpData);
-        string endPoint = "users/sign-up";
-
-        networkManager.PostRequest(endPoint, json, (response) => {
-            if (response == null)
-            {
-                string msg = "[Error] :: Sign-up failed.";
-                Debug.LogError(msg);
-                ToastManager.Instance.ShowToast(msg, MSG_TYPE.ERROR);
-            }
-            else
-            {
-                string msg = "[Success] :: Sign-up completed.";
-                Debug.Log(msg);
-                ToastManager.Instance.ShowToast(msg);
-            }
-        });
-
-        yield return null;
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    private void PingTest()
+
+    public void SetUserData(string userId, string authToken)
     {
-        // 핑 테스트
-        networkManager.GetRequest("ping", (response) => {
-            if (response == null)
-            {
-                string msg = "[Ping failed] :: Server NOT running.";
-                Debug.Log(msg);
-                ToastManager.Instance.ShowToast(msg, MSG_TYPE.ERROR);
-            }
-            else
-            {
-                string msg = "[Ping Test] Server is running!";
-                Debug.Log(msg);
-                ToastManager.Instance.ShowToast(msg);
-            }
-        });
+        UserId = userId;
+        AuthToken = authToken;
+        IsLoggedIn = true;
+    }
+
+    public void ClearUserData()
+    {
+        UserId = null;
+        AuthToken = null;
+        IsLoggedIn = false;
     }
 }
-
-
