c6f76de71bba7c350e8618d2643f2ab6b0f22572
Jaeyoung <jyko1101@gmail.com>
2024-12-06 12:12:56 +0900

    feat: signin response

---
+++ Assets/Scripts/Responses/SignInResponse.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SignInResponse
+{
+    public string Message;
+    public string nickname;
+}
